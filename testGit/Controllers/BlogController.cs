f1209d256556e04ea8d81c66b6c657cb91dea6e8
AhmedMoLotfy <programmerahmedlotfy@gmail.com>
2024-02-22 19:23:00 +0200

    this is a blog controller

---
+++ testGit/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace testGit.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("I am ahmed");
+        }
+    }
+}
