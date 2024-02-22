262aff63c4767e316886a3487166442f815b8839
Habiba El-Sayed <habiba192213@fci.bu.edu.eg>
2024-02-22 19:33:38 +0200

    Habiba's Controller

---
+++ testGit/Controllers/HabibaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace testGit.Controllers
+{
+    public class HabibaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("This is Habiba");
+        }
+    }
+}
